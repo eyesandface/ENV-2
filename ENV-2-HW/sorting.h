89cfbe9c6af05dfedec285abf2d95b2d3d33ce44
eyesandface <sati.gharibyan.00@mail.ru>
2024-10-22 17:21:25 +0400

    Delete ENV-2-HW directory

--- ENV-2-HW/sorting.h
+++
@@ -1,9 +0,0 @@
-#ifndef SORT_COMPLEX_H
-#define SORT_COMPLEX_H
-
-#include "complexNumber.h"
-
-// Function to sort an array of ComplexNumbers based on their absolute value
-void sortComplexNumbers(ComplexNumber arr[], int size);
-
-#endif 
